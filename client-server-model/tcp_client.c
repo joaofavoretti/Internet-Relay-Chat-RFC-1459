d48549a0c8509a577d5f2223790758a50d7b34b7
Joao Favoretti <joaopedrofavoretti@gmail.com>
2022-06-19 09:55:22 -0300

    mudando alguns nomes em tcp_client

--- client-server-model/tcp_client.c
+++ client-server-model/tcp_client.c
@@ -4,79 +4,88 @@ int main(int argc, char *argv[])
 {
   char *program_name = argv[0];
 
+
   if (argc < 3)
   {
-    fprintf(stderr, "Usage: ./%s hostname port\n", program_name);
+    fprintf(stderr, "Usage: ./%s server_ip server_port\n", program_name);
     return 1;
   }
 
-  char *hostname = argv[1];
-  char *port = argv[2];
 
-  logger("Configuring remote address...");
+  char *server_ip = argv[1];
+  char *server_port = argv[2];
+
+
+  logger("Configuring remote address...\n");
   struct addrinfo hints;
   memset(&hints, 0, sizeof(hints));
   hints.ai_socktype = SOCK_STREAM;
-  struct addrinfo *peer_address;
-  if (getaddrinfo(hostname, port, &hints, &peer_address))
+  struct addrinfo *server_address_info;
+  if (getaddrinfo(server_ip, server_port, &hints, &server_address_info))
   {
     fprintf(stderr, "getaddrinfo() failed. (%d)\n", GETSOCKETERRNO());
     return 1;
   }
 
-  logger("Remote address is: ");
+
+  logger("Getting address info...\n");
   char address_buffer[100];
   char service_buffer[100];
-  getnameinfo(peer_address->ai_addr,
-              peer_address->ai_addrlen,
-              address_buffer,
-              sizeof(address_buffer),
-              service_buffer,
-              sizeof(service_buffer),
-              NI_NUMERICHOST);
+  getnameinfo(server_address_info->ai_addr, server_address_info->ai_addrlen, address_buffer,
+              sizeof(address_buffer), service_buffer, sizeof(service_buffer), NI_NUMERICHOST);
+  logger("Remote address is: ");
   logger("%s %s\n", address_buffer, service_buffer);
 
-  logger("Creating socket...");
-  SOCKET socket_peer;
-  socket_peer = socket(peer_address->ai_family,
-                       peer_address->ai_socktype,
-                       peer_address->ai_protocol);
-  if (!ISVALIDSOCKET(socket_peer))
+
+  logger("Creating socket...\n");
+  SOCKET server_socket;
+  server_socket = socket(server_address_info->ai_family, server_address_info->ai_socktype,
+                         server_address_info->ai_protocol);
+  if (!ISVALIDSOCKET(server_socket))
   {
     fprintf(stderr, "socket() failed. (%d)\n", GETSOCKETERRNO());
     return 1;
   }
 
+
   logger("Connecting...\n");
-  if (connect(socket_peer,
-              peer_address->ai_addr,
-              peer_address->ai_addrlen))
+  if (connect(server_socket, server_address_info->ai_addr, server_address_info->ai_addrlen))
   {
     fprintf(stderr, "connect() failed. (%d)\n", GETSOCKETERRNO());
     return 1;
   }
-  freeaddrinfo(peer_address);
+  freeaddrinfo(server_address_info);
+
 
   logger("Connected.\n");
-  logger("To send data, enter text followed by enter.\n");
+
+
+  fd_set master;
+  SOCKET max_socket = server_socket;
+  FD_ZERO(&master);
+  FD_SET(server_socket, &master);
+  FD_SET(fileno(stdin), &master);
+
+
+  printf("To send data, enter text followed by enter.\n");
+
 
   while (1)
   {
-    fd_set reads;
-    FD_ZERO(&reads);
-    FD_SET(socket_peer, &reads);
-    FD_SET(fileno(stdin), &reads);
+    fd_set readfds = master;
 
-    if (select(socket_peer + 1, &reads, 0, 0, 0) < 0)
+
+    if (select(max_socket + 1, &readfds, 0, 0, 0) < 0)
     {
       fprintf(stderr, "select() failed. (%d)\n", GETSOCKETERRNO());
       return 1;
     }
+  
 
-    if (FD_ISSET(socket_peer, &reads))
+    if (FD_ISSET(server_socket, &readfds))
     {
       char read[4096];
-      int bytes_received = recv(socket_peer, read, 4096, 0);
+      int bytes_received = recv(server_socket, read, 4096, 0);
       if (bytes_received < 1)
       {
         logger("Connection closed by peer.\n");
@@ -86,19 +95,19 @@ int main(int argc, char *argv[])
              bytes_received, bytes_received, read);
     }
 
-    if (FD_ISSET(fileno(stdin), &reads))
+    if (FD_ISSET(fileno(stdin), &readfds))
     {
       char read[4096];
       if (!fgetstring(read, 4096, stdin))
         break;
       logger("Sending: %s\n", read);
-      int bytes_sent = send(socket_peer, read, strlen(read), 0);
+      int bytes_sent = send(server_socket, read, strlen(read), 0);
       logger("Sent %d bytes.\n", bytes_sent);
     }
   }
 
   logger("Closing socket...\n");
-  CLOSESOCKET(socket_peer);
+  CLOSESOCKET(server_socket);
 
   logger("Finished.\n");
   return 0;
